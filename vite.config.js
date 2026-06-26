9950dea8ee2f8a44f9fab76bc9551b6bde0d54bd
W3irdogirl <linaaa3103@gmail.com>
2026-06-27 00:23:17 +0200

    some update i modified the origin path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/indieanimation/',
+  //base: '/indieanimation/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
